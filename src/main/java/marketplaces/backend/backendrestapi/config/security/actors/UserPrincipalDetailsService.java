f390b6b8b94d3031d14e6748cb37fc52478b170e
mohamedbrahimi <mohamedaissabrahimi@gmail.com>
2019-06-29 01:01:37 +0100

    - add  UserPrincipalDetailsService to security configuration

---
+++ src/main/java/marketplaces/backend/backendrestapi/config/security/actors/UserPrincipalDetailsService.java
@@ -0,0 +1,37 @@
+package marketplaces.backend.backendrestapi.config.security.actors;
+
+import marketplaces.backend.backendrestapi.config.exceptions.ApiExceptionMessage;
+import marketplaces.backend.backendrestapi.config.exceptions.constants.ExceptionMessages;
+import marketplaces.backend.backendrestapi.config.exceptions.custom.ApiRequestException;
+import marketplaces.backend.backendrestapi.restapi.src.system.user.User;
+import marketplaces.backend.backendrestapi.restapi.src.system.user.UserRepository;
+import marketplaces.backend.backendrestapi.config.exceptions.constants.ExceptionMessages;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
+public class UserPrincipalDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public UserPrincipalDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userRepository.findByUsername(s);
+
+        // return exception if username not found
+
+        User user = Optional.empty().equals(optionalUser) ? null : optionalUser.get();
+        UserPrincipal userPrincipal = new UserPrincipal(user);
+
+        return userPrincipal;
+    }
+}
