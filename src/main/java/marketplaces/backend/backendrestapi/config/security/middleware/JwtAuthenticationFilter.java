f38adda31fe9f13aba3ca0d75aee92345bc09fb5
mohamedbrahimi <mohamedaissabrahimi@gmail.com>
2019-06-29 01:08:15 +0100

    - implement JwtAuth filter like a middleware witch make login throw existing credentials user be possible, and assure the process to return jwt token after a success login.

--- src/main/java/marketplaces/backend/backendrestapi/config/security/middleware/JwtAuthenticationFilter.java
+++ src/main/java/marketplaces/backend/backendrestapi/config/security/middleware/JwtAuthenticationFilter.java
@@ -1,56 +1,78 @@
 package marketplaces.backend.backendrestapi.config.security.middleware;
+
+import marketplaces.backend.backendrestapi.config.security.actors.LoginViewModel;
+import marketplaces.backend.backendrestapi.config.security.actors.UserPrincipal;
 import marketplaces.backend.backendrestapi.config.security.constants.SecurityConstants;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Collection;
 import java.util.Date;
-import java.util.stream.Collectors;
-public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-    private final AuthenticationManager authenticationManager;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.servlet.ServletException;
+import javax.swing.plaf.synth.SynthEditorPaneUI;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private AuthenticationManager authenticationManager;
 
     public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
+    }
 
-        setFilterProcessesUrl(SecurityConstants.AUTH_LOGIN_URL);
+    /* Trigger when we issue POST request to /login
+    We also need to pass in {"username":"dan", "password":"dan123"} in the request body
+     */
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        // Grab credentials and map them to login viewmodel
+        LoginViewModel credentials = null;
+        try {
+            credentials = new ObjectMapper().readValue(request.getInputStream(), LoginViewModel.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        // Create login token
+        System.out.println(credentials.getUsername());
+        System.out.println(credentials.getPassword());
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                credentials.getUsername(),
+                credentials.getPassword(),
+                new ArrayList<>());
+
+        // Authenticate user
+        Authentication auth = authenticationManager.authenticate(authenticationToken);
+
+        return auth;
     }
 
     @Override
-    public Authentication attemptAuthentication(HttpServletRequest request,
-                                                HttpServletResponse response) throws AuthenticationException {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        Authentication authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        // Grab principal
+        UserPrincipal principal = (UserPrincipal) authResult.getPrincipal();
 
-        return authenticationManager.authenticate(authenticationToken);
-    }
-
-    @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
-                                            FilterChain filterChain, Authentication authentication) {
-        User user = ((User) authentication.getPrincipal());
-
-        Collection roles = user.getAuthorities()
+        Collection roles = principal.getAuthorities()
                 .stream()
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.toList());
-
-        System.out.println("roles");
-        System.out.println(user.getUsername());
-        System.out.println(user.getPassword());
-        System.out.println(roles);
+        // Create JWT Token
         byte signingKey[] = SecurityConstants.JWT_SECRET.getBytes();
 
         String token = Jwts.builder()
@@ -58,11 +80,12 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                 .setHeaderParam("typ", SecurityConstants.TOKEN_TYPE)
                 .setIssuer(SecurityConstants.TOKEN_ISSUER)
                 .setAudience(SecurityConstants.TOKEN_AUDIENCE)
-                .setSubject(user.getUsername())
-                .setExpiration(new Date(System.currentTimeMillis() + 864000000))
+                .setSubject(principal.getUsername())
+                .setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
                 .claim("rol", roles)
                 .compact();
 
-        response.addHeader(SecurityConstants.TOKEN_HEADER, SecurityConstants.TOKEN_PREFIX + token);
+        // Add token in response
+        response.addHeader(SecurityConstants.TOKEN_HEADER, SecurityConstants.TOKEN_PREFIX +  token);
     }
 }
