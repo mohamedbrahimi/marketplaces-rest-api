50414787755ead56ffbd4e0ebd3e50bd44942819
mohamedbrahimi <mohamedaissabrahimi@gmail.com>
2019-07-03 22:49:32 +0100

    - Remove unused imports

--- src/main/java/marketplaces/backend/backendrestapi/config/security/actors/UserPrincipalDetailsService.java
+++ src/main/java/marketplaces/backend/backendrestapi/config/security/actors/UserPrincipalDetailsService.java
@@ -1,11 +1,7 @@
 package marketplaces.backend.backendrestapi.config.security.actors;
 
-import marketplaces.backend.backendrestapi.config.exceptions.ApiExceptionMessage;
-import marketplaces.backend.backendrestapi.config.exceptions.constants.ExceptionMessages;
-import marketplaces.backend.backendrestapi.config.exceptions.custom.ApiRequestException;
 import marketplaces.backend.backendrestapi.restapi.src.system.user.User;
 import marketplaces.backend.backendrestapi.restapi.src.system.user.UserRepository;
-import marketplaces.backend.backendrestapi.config.exceptions.constants.ExceptionMessages;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
