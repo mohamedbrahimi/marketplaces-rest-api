a0bf4fe9ed4e6845c26b34dd1b10eb7316bef427
mohamedbrahimi <mohamedaissabrahimi@gmail.com>
2019-07-03 22:56:22 +0100

    - Remove unused imports

--- src/main/java/marketplaces/backend/backendrestapi/restapi/src/system/user/UserController.java
+++ src/main/java/marketplaces/backend/backendrestapi/restapi/src/system/user/UserController.java
@@ -6,12 +6,9 @@ import marketplaces.backend.backendrestapi.config.exceptions.constants.Exception
 import marketplaces.backend.backendrestapi.config.exceptions.custom.ApiRequestException;
 import marketplaces.backend.backendrestapi.config.exceptions.unknown.ApiRequestUnknownException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.sql.rowset.spi.SyncResolver;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,6 +23,7 @@ public class UserController {
 
     @GetMapping
     public List<User> getUsers() {
+
         return userRepository.findAll();
     }
 
