df087eb5e6fa233b4381832499a512f27b4d5b12
mohamedbrahimi <mohamedaissabrahimi@gmail.com>
2019-07-08 19:41:54 +0100

    - used @Query to limit and control selected fields from user collection.

--- src/main/java/marketplaces/backend/backendrestapi/restapi/src/system/user/UserRepository.java
+++ src/main/java/marketplaces/backend/backendrestapi/restapi/src/system/user/UserRepository.java
@@ -1,12 +1,17 @@
 package marketplaces.backend.backendrestapi.restapi.src.system.user;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 public interface UserRepository extends MongoRepository<User, String>{
     Optional<User> findById(String id);
     Optional<User> findByUsername(String username);
     Optional<User> findByMail(String mail);
     Optional<User> findByPhone(String phone);
+
+    @Query(value = "{}", fields = "{username:1, mail:1, id:0}", sort = "{username:1}")
+    List<User> findAll();
 
 }
