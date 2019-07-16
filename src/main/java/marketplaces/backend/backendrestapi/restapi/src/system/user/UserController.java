11ddad1d58ae6cf820b1c62bf9f877517fab6e8b
mohamedbrahimi <mohamedaissabrahimi@gmail.com>
2019-07-16 09:07:05 +0100

    - used filtering class with the new values.

--- src/main/java/marketplaces/backend/backendrestapi/restapi/src/system/user/UserController.java
+++ src/main/java/marketplaces/backend/backendrestapi/restapi/src/system/user/UserController.java
@@ -36,8 +36,8 @@ public class UserController {
 
 
     @GetMapping
-    public Page<User> getUsers(@RequestHeader int size, @RequestHeader int page, @RequestHeader String text ) {
-        return userService.find(new Filtering(size, page, text));
+    public Page<User> getUsers(@RequestHeader int size, @RequestHeader int page, @RequestHeader String text,@RequestHeader int status ) {
+        return userService.find(new Filtering(size, page, text, status));
     }
 
     @GetMapping("/{id}")
@@ -62,30 +62,6 @@ public class UserController {
 
     @PutMapping
     public void update(@RequestBody User user) {
-        user = this.encodePasseord(user);
-        this.CheckIfValidUser(user);
-        this.CheckIfNewUser(user);
-        Query query = new Query();
-        query.addCriteria(Criteria.where("id").is(user.getId()));
-        Update update = new Update();
-        if(user.getUsername() != null) update.set("username",user.getUsername());
-        if(user.getMail() != null) update.set("mail",user.getMail());
-        if(user.getPhone() != null) update.set("phone",user.getPhone());
-        if(user.getPassword() != null) update.set("password",user.getPassword());
-        if(user.getRoles() != null) update.set("roles",user.getRoles());
-        if(user.getAuthorities() != null) update.set("authorities",user.getAuthorities());
-        if(user.getStatus() == 1 || user.getStatus() == 0 ) update.set("status",user.getStatus());
-
-        try {
-            mongoTemplate.findAndModify(query, update, User.class);
-          //  userRepository.save(user);
-        } catch (Exception e) {
-            this.CheckIfValidUser(user);
-            this.CheckIfNewUser(user);
-            System.out.println(e.getMessage());
-
-            this.UnknownException();
-        }
 
     }
 
@@ -95,93 +71,6 @@ public class UserController {
     }
 
 
-    public void CheckIfValidUser(User user) {
 
-        user = (User) user;
-
-        if (user.getId() != null && !user.getId().matches(GlobalConstants.REGEXP_OBJECTID))
-            throw new ApiRequestException(ExceptionMessages.ERROR_OBJECT_ID_NOT_VALID);
-        if (user.getUsername() == null || user.getUsername().length() < 4)
-            throw new ApiRequestException(ExceptionMessages.ERROR_USER_SMALL_THEN_4);
-        if (user.getPassword() == null || user.getPassword().length() < 8)
-            throw new ApiRequestException(ExceptionMessages.ERROR_PASS_SMALL_THEN_8);
-        if (user.getMail() == null || user.getPhone() == null || user.getMail().equals("") || user.getPhone().equals(""))
-            throw new ApiRequestException(ExceptionMessages.ERROR_FIELD_NULL);
-        if (!user.getMail().matches(GlobalConstants.REGEXP_FOR_MAIL_VALDATION))
-            throw new ApiRequestException(ExceptionMessages.ERROR_INVALID_MAIL);
-        if (!user.getPhone().matches(GlobalConstants.REGEXP_FOR_PHONE_NATIONAL_FORMAT))
-            throw new ApiRequestException(ExceptionMessages.ERROR_INVALID_PHONE_NUMBER);
-    }
-
-
-    public void CheckIfNewUser(User user) {
-
-        User newUser = (User) user;
-        Optional<User> optionalUser = user.getId() == null ? Optional.empty() : userRepository.findById(user.getId());
-
-        if (!optionalUser.equals(Optional.empty())) {
-
-            if (!optionalUser.get().getUsername().equals(newUser.getUsername()) && !userRepository.findByUsername(newUser.getUsername()).equals(Optional.empty())) {
-                throw new ApiRequestException(ExceptionMessages.ERROR_EXISTING_USERNAME);
-            }
-            if (!optionalUser.get().getMail().equals(newUser.getMail()) && !userRepository.findByMail(newUser.getMail()).equals(Optional.empty())) {
-                throw new ApiRequestException(ExceptionMessages.ERROR_EXISTING_MAIL);
-            }
-            if (!optionalUser.get().getPhone().equals(newUser.getPhone()) && !userRepository.findByPhone(newUser.getPhone()).equals(Optional.empty())) {
-                throw new ApiRequestException(ExceptionMessages.ERROR_EXISTING_PHONE);
-            }
-        } else if (user.getId() == null) {
-
-            if (!Optional.empty().equals(userRepository.findByUsername(newUser.getUsername()))) {
-                throw new ApiRequestException(ExceptionMessages.ERROR_EXISTING_USERNAME);
-            }
-
-            if (!userRepository.findByMail(newUser.getMail()).equals(Optional.empty())) {
-                throw new ApiRequestException(ExceptionMessages.ERROR_EXISTING_MAIL);
-            }
-
-
-            if (!userRepository.findByPhone(newUser.getPhone()).equals(Optional.empty())) {
-                throw new ApiRequestException(ExceptionMessages.ERROR_EXISTING_PHONE);
-            }
-        } else {
-            throw new ApiRequestException(ExceptionMessages.ERROR_DOCUMENT_NOT_FOUND);
-        }
-
-
-    }
-
-
-    public void UnknownException() {
-        throw new ApiRequestUnknownException(ExceptionMessages.ERROR_UNKNOWN_EXCEPTION);
-    }
-
-
-    public User encodePasseord(User user) {
-        // a lot lines of codes to test if we need to encode the password
-        // Need to minimize this by mongo hooks
-
-        if (user.getPassword() == null)
-            throw new ApiRequestException(ExceptionMessages.ERROR_FIELD_NULL);
-
-        if (user.getId() == null) {
-            user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
-        }
-        else {
-
-            Optional<User> oldUser = userRepository.findById(user.getId());
-            String oldPassword = oldUser.equals(Optional.empty()) ? null : oldUser.get().getPassword();
-            if (oldPassword == null) {
-
-                user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
-            } else if (!new BCryptPasswordEncoder().matches(user.getPassword(), oldUser.get().getPassword())) {
-                user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
-            }
-
-        }
-
-        return user;
-
-    }
 
 }
