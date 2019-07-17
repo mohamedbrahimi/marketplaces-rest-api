991e41aaaa3c0fef621954fa67fa9080dc4a8c7b
mohamedbrahimi <mohamedaissabrahimi@gmail.com>
2019-07-17 09:09:10 +0100

    - upgrade audit class to be more specific.

--- src/main/java/marketplaces/backend/backendrestapi/restapi/src/system/user/UserController.java
+++ src/main/java/marketplaces/backend/backendrestapi/restapi/src/system/user/UserController.java
@@ -1,22 +1,9 @@
 package marketplaces.backend.backendrestapi.restapi.src.system.user;
-
-import marketplaces.backend.backendrestapi.config.global.GlobalConstants;
-
-import marketplaces.backend.backendrestapi.config.exceptions.constants.ExceptionMessages;
-import marketplaces.backend.backendrestapi.config.exceptions.custom.ApiRequestException;
-import marketplaces.backend.backendrestapi.config.exceptions.unknown.ApiRequestUnknownException;
 import marketplaces.backend.backendrestapi.config.global.filtering.Filtering;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import java.util.List;
 import java.util.Optional;
 
 
@@ -42,27 +29,17 @@ public class UserController {
 
     @GetMapping("/{id}")
     public Optional<User> getUser(@PathVariable String id) {
-
         return userRepository.findById(id);
     }
 
     @PostMapping
     public void insert(@RequestBody User user) {
-
-        try {
-            user = userService.encodePasseord(user);
-            userRepository.insert(user);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            userService.CheckIfValidUser(user);
-            userService.CheckIfNewUser(user);
-            userService.UnknownException(e.getMessage());
-        }
+       userService.insert(user);
     }
 
     @PutMapping
     public void update(@RequestBody User user) {
-
+       userService.update(user);
     }
 
     @DeleteMapping("/{id}")
