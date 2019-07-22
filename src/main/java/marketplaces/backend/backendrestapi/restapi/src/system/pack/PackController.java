7e4abfd01fc8181f14c1ef84d5823f0b49c37d49
mohamedbrahimi <mohamedaissabrahimi@gmail.com>
2019-07-22 22:36:34 +0100

    - implemented PackService.

--- src/main/java/marketplaces/backend/backendrestapi/restapi/src/system/pack/PackController.java
+++ src/main/java/marketplaces/backend/backendrestapi/restapi/src/system/pack/PackController.java
@@ -1,6 +1,8 @@
 package marketplaces.backend.backendrestapi.restapi.src.system.pack;
 
+import marketplaces.backend.backendrestapi.config.global.filtering.Filtering;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,11 +13,17 @@ import java.util.Optional;
 public class PackController {
 
     @Autowired
+    PackService packService;
+    @Autowired
     PackRepository packRepository;
 
     @GetMapping
-    public List<Pack> getPacks(){
-        return packRepository.findAll();
+    public Page<Pack> getPacks(
+            @RequestHeader int size,
+            @RequestHeader int page,
+            @RequestHeader String text,
+            @RequestHeader int status){
+        return packService.find(new Filtering(size, page, text, status));
     }
 
     @GetMapping("/{id}")
@@ -25,17 +33,17 @@ public class PackController {
 
     @PostMapping
     public void insert(@RequestBody Pack pack){
-        packRepository.insert(pack);
+        packService.insert(pack);
     }
 
     @PutMapping
     public void update(@RequestBody Pack pack){
-        packRepository.save(pack);
+        packService.update(pack);
     }
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable String id){
-        packRepository.deleteById(id);
+        packService.delete(id);
     }
 
 }
