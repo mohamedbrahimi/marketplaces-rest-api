d10f95fde965367f14afd5234750a956bbdd3c24
mohamedbrahimi <mohamedaissabrahimi@gmail.com>
2019-07-22 22:37:55 +0100

    - updated

--- src/main/java/marketplaces/backend/backendrestapi/restapi/src/system/user/UserRepository.java
+++ src/main/java/marketplaces/backend/backendrestapi/restapi/src/system/user/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.mongodb.repository.Query;
 import java.util.List;
 import java.util.Optional;
 public interface UserRepository extends MongoRepository<User, String>{
+
     Optional<User> findById(String id);
     Optional<User> findByUsername(String username);
     Optional<User> findByMail(String mail);
