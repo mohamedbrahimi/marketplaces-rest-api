4a63666352343d017617420553df8754759fde05
mohamedbrahimi <mohamedaissabrahimi@gmail.com>
2019-07-28 23:16:05 +0100

    - Updated Team repository.

--- src/main/java/marketplaces/backend/backendrestapi/restapi/src/system/team/TeamRepository.java
+++ src/main/java/marketplaces/backend/backendrestapi/restapi/src/system/team/TeamRepository.java
@@ -2,6 +2,8 @@ package marketplaces.backend.backendrestapi.restapi.src.system.team;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface TeamRepository extends MongoRepository<Team, String> {
+import java.util.Optional;
 
+public interface TeamRepository extends MongoRepository<Team, String> {
+    Optional<Team> findByCode(String code);
 }
